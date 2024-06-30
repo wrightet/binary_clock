736d02060d11516773f992664a86a207dd681eec
Ethan Wright <wrightet707@gmail.com>
2024-06-29 23:12:26 -0700

    get the buttons to workupdating the hour and minutes in the led strip and on the lcd screen

--- clock_with_RTC_prints_to_lcd/clock_with_led_strips/clock_with_RTC_prints_to_lcd.ino
+++ clock_with_RTC_prints_to_lcd/clock_with_led_strips/clock_with_RTC_prints_to_lcd.ino
@@ -2,6 +2,7 @@
 #include <DS3231.h>
 #include <Wire.h>
 #include <FastLED.h>
+
 // Init the DS3231 using the hardware interface
 DS3231 rtc(SDA, SCL);
 
@@ -11,19 +12,25 @@ const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 // Pins in order of most to least significant bit
-#define min_ pin = 6;
-#define hour_pin = 7;
-#define NUM_LEDS 6;
+const int min_pin = 6;
+const int hour_pin = 7;
+const int NUM_LEDS = 6;
+
 // Create a FastLED object
 CRGB min_leds[NUM_LEDS];
 CRGB hour_leds[NUM_LEDS];
 
 void parseTimeString(const String &timeString, int &hour, int &minute);
 
-int hour = 0;
-int minute = 0;
+int manualHour = 0;
+int manualMinute = 0;
 
-void setup() {
+const int buttonPins[] = {47, 49, 51, 53}; // Pins where buttons are connected
+
+bool timeAdjusted = false;
+
+void setup()
+{
     // Start the serial port
     Serial.begin(57600);
 
@@ -40,55 +47,104 @@ void setup() {
     rtc.begin();
 
     // The following lines can be uncommented to set the date and time
-    rtc.setDOW(TUESDAY);       // Set Day-of-Week to FRIDAY
-    rtc.setTime(11, 10, 0);   // Set the time to 22:21:00 (24hr format)
-    rtc.setDate(18, 6, 2024); // Set the date to May 17th, 2024
+    rtc.setDOW(SATURDAY);     // Set Day-of-Week to TUESDAY
+    rtc.setTime(10, 43, 55);  // Set the time to 10:43:00 (24hr format)
+    rtc.setDate(24, 6, 2024); // Set the date to June 24th, 2024
+
+    // Initialize button pins as inputs
+    for (int i = 0; i < 4; i++)
+    {
+        pinMode(buttonPins[i], INPUT);
+    }
 
     // Initialize the LCD
     lcd.begin(16, 2);
 
     // Delay for initial setup
     delay(1000);
+
+    // Initialize manual time with RTC time
+    String timeString = rtc.getTimeStr();
+    parseTimeString(timeString, manualHour, manualMinute);
 }
 
-void loop() {
+void loop()
+{
     // Get the current time as a string
     String timeString = rtc.getTimeStr();
     Serial.println(timeString);
 
-    // Parse the hour and minute from the time string
-    parseTimeString(timeString, hour, minute);
+    // Handle button presses
+    if (digitalRead(buttonPins[0]) == HIGH)
+    {                                            // Button pressed (HIGH due to pull-down resistor)
+        manualHour = (manualHour - 1 + 24) % 24; // Wrap around 24-hour format
+        delay(200);                              // Debounce delay
+        timeAdjusted = true;
+    }
+    if (digitalRead(buttonPins[1]) == HIGH)
+    {                                       // Button pressed (HIGH due to pull-down resistor)
+        manualHour = (manualHour + 1) % 24; // Wrap around 24-hour format
+        delay(200);                         // Debounce delay
+        timeAdjusted = true;
+    }
+    if (digitalRead(buttonPins[2]) == HIGH)
+    {                                                // Button pressed (HIGH due to pull-down resistor)
+        manualMinute = (manualMinute - 1 + 60) % 60; // Wrap around 60-minute format
+        delay(200);                                  // Debounce delay
+        timeAdjusted = true;
+    }
+    if (digitalRead(buttonPins[3]) == HIGH)
+    {                                           // Button pressed (HIGH due to pull-down resistor)
+        manualMinute = (manualMinute + 1) % 60; // Wrap around 60-minute format
+        delay(200);                             // Debounce delay
+        timeAdjusted = true;
+    }
+
+    if (timeAdjusted)
+    {
+        rtc.setTime(manualHour, manualMinute, 0); // Update the RTC time
+        timeAdjusted = false;                     // Reset the flag
+    }
+
+    // Parse the hour and minute from the RTC time
+    int rtcHour, rtcMinute;
+    parseTimeString(timeString, rtcHour, rtcMinute);
 
     // Display the parsed values
-    Serial.print("Parsed hour: ");
-    Serial.println(hour);
-    Serial.print("Parsed minute: ");
-    Serial.println(minute);
+    Serial.print("RTC hour: ");
+    Serial.println(rtcHour);
+    Serial.print("RTC minute: ");
+    Serial.println(rtcMinute);
 
     // Display the binary values on LEDs
-    displayBinary(hour, hour_leds);
-    displayBinary(minute, hour_leds);
+    displayNumberInBinary(rtcHour, hour_leds, CRGB::Green);
+    displayNumberInBinary(rtcMinute, min_leds, CRGB::Red);
 
-    // Display the time on the LCD
+    // Display the current time from RTC on the LCD
     lcd.setCursor(0, 0);
     lcd.print("Time:  ");
     lcd.print(timeString);
 
-    lcd.setCursor(0, 1);
-    lcd.print("Date: ");
-    lcd.print(rtc.getDateStr());
-
-   
+    // // Display the manually adjusted time on the LCD
+    // lcd.setCursor(0, 1);
+    // lcd.print("Manual: ");
+    // if (manualHour < 10)
+    //     lcd.print("0"); // Leading zero for single digit hours
+    // lcd.print(manualHour);
+    // lcd.print(":");
+    // if (manualMinute < 10)
+    //     lcd.print("0"); // Leading zero for single digit minutes
+    // lcd.print(manualMinute);
 }
 
-void displayNumberInBinary(int number, leds)
+void displayNumberInBinary(int number, CRGB *leds, CRGB color)
 {
     for (int i = 0; i < NUM_LEDS; i++)
     {
         // Check if the ith bit is set
         if (number & (1 << i))
         {
-            leds[i] = CRGB::Red; // Turn LED red (1)
+            leds[i] = color; // Turn LED to the specified color (1)
         }
         else
         {
@@ -99,7 +155,8 @@ void displayNumberInBinary(int number, leds)
 }
 
 // Function to parse time string and update hour and minute variables
-void parseTimeString(const String &timeString, int &hour, int &minute) {
+void parseTimeString(const String &timeString, int &hour, int &minute)
+{
     // Extract hour and minute from the string
     hour = timeString.substring(0, 2).toInt();
     minute = timeString.substring(3, 5).toInt();
